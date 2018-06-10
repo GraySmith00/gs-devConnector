7916cd92a8605bc6acdaf4f4009e0453d8926e85
Gray Smith <gray.smith00@gmail.com>
2018-06-10 13:01:44 -0600

    Add basic registration validation, name must be between 2 and 30 characters

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+
 // Load User Model
 const User = require('../../models/User');
 
@@ -18,13 +21,16 @@ router.get('/test', (req, res) => res.json({ msg: 'users works!' }));
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   User.findOne({
     email: req.body.email
   }).then(user => {
     if (user) {
-      return res.status(400).json({
-        email: 'Email already exist'
-      });
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', // Size
