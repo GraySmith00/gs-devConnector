167ca1d8e300a3dab9c33ca243755016fa51e23d
Gray Smith <gray.smith00@gmail.com>
2018-06-10 18:38:33 -0600

    Fix profile model bugs and add get current user profile route

--- models/Profile.js
+++ models/Profile.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Scehma;
+const Schema = mongoose.Schema;
 
 // Create Schema
 const ProfileSchema = new Schema({
@@ -67,7 +67,7 @@ const ProfileSchema = new Schema({
   experience: [
     {
       school: {
-        String,
+        type: String,
         required: true
       },
       degree: {
