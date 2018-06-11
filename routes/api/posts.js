770155321c088c53fe459e6d86bac0107035c91c
Gray Smith <gray.smith00@gmail.com>
2018-06-11 10:56:45 -0600

    Add route for deleting posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 
 // Load Model
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 // Load Validation
 const validatePostInput = require('../../validation/post');
@@ -38,6 +39,55 @@ router.post(
 
     // save post and return json
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   GET api/posts
+// @desc    Get posts index page
+// @access  Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: 'No post found with that ID' })
+    );
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post by id
+// @access  Private
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        // Check if current user owns post
+        if (post.user.toString() !== req.user.id) {
+          return res
+            .status(401)
+            .json({ notauthorized: 'You are not authorized for that!' });
+        }
+
+        // Remove post
+        post
+          .remove()
+          .then(() => res.json({ success: true }))
+          .catch(err =>
+            res.status(404).json({ postnotfound: 'Post not found' })
+          );
+      });
+    });
   }
 );
 
