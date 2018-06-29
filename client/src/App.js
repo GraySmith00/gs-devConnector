84a7be4fd6afafdcf6241f2b53614c27e836d069
Gray Smith <gray.smith00@gmail.com>
2018-06-28 22:26:33 -0600

    Add basic user registration functionality combining front and back end, fix css scroll bug

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Route exact path="/" component={Landing} />
-          <div className="container">
+          <div className="container main-container">
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
           </div>
