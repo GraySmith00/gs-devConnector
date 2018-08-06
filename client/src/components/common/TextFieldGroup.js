7bf78556ac25aec217acb43b9e7156efa9c3c3e4
Gray Smith <gray.smith00@gmail.com>
2018-08-05 18:27:46 -0600

    Refactor input form groups into TextFieldGroup component

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
 
 const TextFieldGroup = ({
   name,
@@ -11,7 +13,38 @@ const TextFieldGroup = ({
   onChange,
   disabled
 }) => {
-  return <div />;
+  return (
+    <div className="form-group">
+      <input
+        type={type}
+        className={classnames('form-control form-control-lg', {
+          'is-invalid': error
+        })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      {info && <small className="form-text text-muted">{info}</small>}
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+TextFieldGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  disabled: PropTypes.string
+};
+
+TextFieldGroup.defaultProps = {
+  type: 'text'
 };
 
 export default TextFieldGroup;
