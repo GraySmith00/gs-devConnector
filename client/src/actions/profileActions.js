76538e75946c7c6b4fb7a9cea4c638f6aa4e6816
Gray Smith <gray.smith00@gmail.com>
2018-08-07 09:37:02 -0600

    Add ability to delete account and edit profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -3,7 +3,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
-  GET_ERRORS
+  GET_ERRORS,
+  SET_CURRENT_USER
 } from './types';
 
 // Get current profile
@@ -50,4 +51,28 @@ export const clearCurrentProfile = () => {
   return {
     type: CLEAR_CURRENT_PROFILE
   };
+};
+
+// Delete Account & Profile
+export const deleteAccount = () => dispatch => {
+  if (
+    window.confirm(
+      'Are you sure you want to delete your account? This cannot be undone!!!'
+    )
+  ) {
+    axios
+      .delete('api/profile')
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
 };
