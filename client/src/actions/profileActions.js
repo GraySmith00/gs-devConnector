87f14d6e27ba2ff82528bd892b11da710d1c326a
Gray Smith <gray.smith00@gmail.com>
2018-08-08 15:07:35 -0600

    Get profile by handle working in redux state, but not yet displayed

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -27,6 +27,25 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+// Get Profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
+
 // Create profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
@@ -59,12 +78,12 @@ export const addExperience = (expData, history) => dispatch => {
   axios
     .post('api/profile/experience', expData)
     .then(res => history.push('/dashboard'))
-    .catch(err => {
+    .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      });
-    });
+      })
+    );
 };
 
 // Add Education
@@ -72,48 +91,48 @@ export const addEducation = (eduData, history) => dispatch => {
   axios
     .post('api/profile/education', eduData)
     .then(res => history.push('/dashboard'))
-    .catch(err => {
+    .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      });
-    });
+      })
+    );
 };
 
 // Delete Experience
 export const deleteExperience = id => dispatch => {
   axios
     .delete(`api/profile/experience/${id}`)
-    .then(res => {
+    .then(res =>
       dispatch({
         type: GET_PROFILE,
         payload: res.data
-      });
-    })
-    .catch(err => {
+      })
+    )
+    .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      });
-    });
+      })
+    );
 };
 
 // Delete Education
 export const deleteEducation = id => dispatch => {
   axios
     .delete(`api/profile/education/${id}`)
-    .then(res => {
+    .then(res =>
       dispatch({
         type: GET_PROFILE,
         payload: res.data
-      });
-    })
-    .catch(err => {
+      })
+    )
+    .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      });
-    });
+      })
+    );
 };
 
 // Get all profiles
@@ -121,18 +140,18 @@ export const getProfiles = () => dispatch => {
   dispatch(setProfileLoading());
   axios
     .get('/api/profile/all')
-    .then(res => {
+    .then(res =>
       dispatch({
         type: GET_PROFILES,
         payload: res.data
-      });
-    })
-    .catch(err => {
+      })
+    )
+    .catch(err =>
       dispatch({
         type: GET_PROFILES,
         payload: null
-      });
-    });
+      })
+    );
 };
 
 // Delete Account & Profile
