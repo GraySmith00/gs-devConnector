21c48cc64ca7d409ede45028ae16163cfbfb1bef
Gray Smith <gray.smith00@gmail.com>
2018-08-10 18:28:03 -0600

    Modify server.js and root package.json for deployment

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
+const app = express();
+
+// API Routes
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
-const app = express();
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
